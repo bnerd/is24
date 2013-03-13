b86e0945c793f15da2ea3b4fbdb0f8a51ce41db0
Bernd Suenkel <bsuenkel@bnerd.de>
2013-03-13 13:32:31 +0100

    added simple auth helpers

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ client = Is24::Api.new(
 )
 ```
 
-Get expose for object
+Get exposé for object
 
 ```ruby
 expose = client.expose(ID)
@@ -34,7 +34,60 @@ puts expose
 puts expose.contactDetails.address
 ```
 
-### Contributing to is24
+## Authentication
+
+IS24 uses oauth v1.0.
+
+### Step 1: Get a request token
+
+Set up a new client
+
+```ruby
+client = Is24::Api.new(
+  consumer_key: YOUR_CONSUMER_KEY,
+  consumer_secret: YOUR_CONSUMER_SECRET
+)
+```
+
+and request a request_token providing a callback uri to your app
+
+```ruby
+request_token = client.request_token(YOUR_CALLBACK_URI)
+
+## returns
+# {
+#  :oauth_token => OAUTH_TOKEN,
+#  :oauth_token_secret => OAUTH_TOKEN_SECRET,
+#  :redirect_uri => "http://rest.immobilienscout24.de/restapi/security/oauth/confirm_access?oauth_token=OAUTH_TOKEN"
+# }
+```
+
+Redirect your user to the redirect_uri to authorize your request.
+
+### Step 2: Request access token
+
+Once the user has authorized your request you'll receive a get request to your
+callback. Parse the response params and grab your tokens:
+':
+
+```ruby
+tokens = client.request_access_token(
+  oauth_token: OAUTH_TOKEN
+  oauth_token_secret: OAUTH_TOKEN_SECRET,
+  oauth_verifier: OAUTH_VERIFIER )
+
+## returns
+# {
+#  :oauth_token=> OAUTH_TOKEN,
+#  :oauth_token_secret=> OAUTH_TOKEN_SECRET
+# }
+```
+
+Until the user revokes permission to your app, the access_token is valid for an unlimited time.
+
+Store the tokens and reuse them for subsequent calls to restricted resources of the REST API.
+
+## Contributing to is24
  
 * Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet.
 * Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it.
--- lib/is24.rb
+++ lib/is24.rb
@@ -2,22 +2,59 @@ require 'is24/logger'
 require 'faraday'
 require 'faraday_middleware'
 require 'faraday_middleware/response/mashify'
+require 'cgi'
 
 module Is24
   class Api
     include Is24::Logger
 
     API_ENDPOINT = "http://rest.immobilienscout24.de/restapi/api/search/v1.0/"
+    API_AUTHORIZATION_ENDPOINT = "http://rest.immobilienscout24.de/restapi/security/"
 
     def initialize( options = {} )
-      logger "Initialized Bnerd IS24 with options #{options}"
+      logger "Initialized b'nerd IS24 with options #{options}"
       
       @token = options[:token] || nil
       @secret = options[:secret] || nil
       @consumer_secret = options[:consumer_secret] || nil
       @consumer_key = options[:consumer_key] || nil
       
-      raise "Invalid Credentials" if @secret.nil? || @token.nil?
+      raise "Missing Credentials!" if @consumer_secret.nil? || @consumer_key.nil?
+    end
+    
+    def request_token( callback_uri )
+      # TODO error handling
+      response = connection(:authorization, callback_uri).get("oauth/request_token")
+      
+      body = response.body.split('&')
+      response = {
+        :oauth_token => CGI::unescape(body[0].split("=")[1]),
+        :oauth_token_secret => CGI::unescape(body[1].split("=")[1]),
+        :redirect_uri => "http://rest.immobilienscout24.de/restapi/security/oauth/confirm_access?#{body[0]}"
+      }
+    end
+
+    def request_access_token( params = {} )
+      # TODO error handling
+      @oauth_verifier = params[:oauth_verifier]
+      @token = params[:oauth_token]
+      @secret = params[:oauth_token_secret]
+
+      response = connection(:authorization).get("oauth/access_token")
+      puts response.inspect
+      body = response.body.split('&')
+      
+      response = {
+        :oauth_token => body[0].split('=')[1],
+        :oauth_token_secret => CGI::unescape(body[1].split('=')[1]),
+      }
+      
+      # set credentials in client
+      @token = response[:oauth_token]
+      @token_secret = response[:oauth_token_secret]
+      
+      # return access token and secret
+      response
     end
     
     def search(options)
@@ -40,23 +77,46 @@ module Is24
     
     protected
     
-    def connection
-      Faraday::Connection.new(
+    def connection(connection_type = :default, callback_uri = nil)
+    
+      # set request defaults
+      defaults = {
         :url => API_ENDPOINT,
         :accept =>  'application/json',
         :headers => {
           :accept =>  'application/json',
-          :user_agent => 'b\'nerd .media IS24 Ruby Client'} ) do |builder|
-            builder.request :oauth, {
-              :consumer_key => @consumer_key, 
-              :consumer_secret => @consumer_secret, 
-              :token => @token, 
-              :token_secret => @secret
-            }
+          :user_agent => 'b\'nerd .media IS24 Ruby Client'}      
+      }
+      
+      defaults.merge!( {
+        :url => API_AUTHORIZATION_ENDPOINT
+      } ) if connection_type =~ /authorization/i
+      
+      # define oauth credentials
+      oauth = {
+        :consumer_key => @consumer_key, 
+        :consumer_secret => @consumer_secret, 
+        :token => @token, 
+        :token_secret => @secret
+      }
+      
+      # merge callback_uri if present
+      oauth.merge!( {
+        :callback => callback_uri
+      } ) if connection_type =~ /authorization/i && callback_uri
+      
+      # merge verifier if present
+      oauth.merge!( {
+        :verifier => @oauth_verifier
+      } ) if connection_type =~ /authorization/i && @oauth_verifier
+      
+      Faraday::Connection.new( defaults ) do |builder|
+            builder.request :oauth, oauth
             builder.response :mashify
-            builder.response :json
+            builder.response :json unless connection_type =~ /authorization/i
             builder.adapter Faraday.default_adapter
           end
     end
+    
   end
 end  
